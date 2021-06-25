4ec039dd56d002410adfd3e9cb955628ecd04fb8
yosuke ota <otameshiyo23@gmail.com>
2021-06-25 19:28:01 +0900

    Fixed false positives for trailing comma with import in indent rule.

---
+++ tests/fixtures/rules/indent/invalid/ts/ts-union01-input.svelte
@@ -0,0 +1,56 @@
+<!-- prettier-ignore -->
+<script lang="ts">
+type A =
+| a
+| b
+| c
+| d
+
+
+type T =
+a
+|
+b
+type U =
+|
+a
+|
+b
+type V =
+a
+&
+b
+type W =
+&
+a
+&
+b
+type X =
+&
+(
+a
+|
+b
+)
+
+type T =
+a
+| b
+type U =
+| a
+| b
+type V =
+a
+& b
+type W =
+& a
+& b
+type X =
+&
+(
+a
+| b
+)
+</script>
+
+<!--tests/fixtures/rules/indent/invalid/ts/ts-union01-input.svelte-->
