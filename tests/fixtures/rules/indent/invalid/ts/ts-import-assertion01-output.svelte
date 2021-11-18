f494108332b6d03c619a652fa621927f3b239e0e
yosuke ota <otameshiyo23@gmail.com>
2021-11-18 12:25:49 +0900

    Update indent rule to support typescript v4.5

---
+++ tests/fixtures/rules/indent/invalid/ts/ts-import-assertion01-output.svelte
@@ -0,0 +1,11 @@
+<!-- prettier-ignore -->
+<script lang="ts">
+  import foo from "./foo.json" assert
+    {
+      type
+        :
+        "json"
+    }
+</script>
+
+<!--tests/fixtures/rules/indent/invalid/ts/ts-import-assertion01-input-->
---
+++ tests/fixtures/rules/indent/invalid/ts/ts-type-only-import-export02-output.svelte
@@ -0,0 +1,25 @@
+<!-- prettier-ignore -->
+<script lang="ts">
+  import
+  {
+    type
+    Foo
+  }
+  from
+    'foo';
+  export
+  {
+    type
+    Bar
+  }
+  from
+    'bar';
+  export
+  {
+    type
+    Baz
+  }
+  ;
+</script>
+
+<!--tests/fixtures/rules/indent/invalid/ts/ts-type-only-import-export02-input.svelte-->
