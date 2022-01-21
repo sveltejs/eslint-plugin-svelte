a86b9ad36b6181825193fafc29070247533fec72
ota-meshi <otameshiyo23@gmail.com>
2022-01-21 11:18:34 +0900

    Fix false positives for import brace in indent rule

---
+++ tests/fixtures/rules/indent/invalid/import-declaration01-output.svelte
@@ -0,0 +1,12 @@
+<!-- prettier-ignore -->
+<script>
+  import Def, {
+    foo
+  } from 'mod';
+  import
+    Def2, {
+      bar
+    } from 'mod';
+</script>
+
+<!-- import-declaration01-input.svelte -->
