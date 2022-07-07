1b193f6ebd4fb40bf5ea510eb0d6b52fe23b7087
yosuke ota <otameshiyo23@gmail.com>
2022-07-07 09:00:54 +0900

    Chore: update svelte-kit

--- svelte.config.esm.mjs
+++ svelte.config.esm.mjs
@@ -1,10 +1,6 @@
 /* global __dirname, URL -- __dirname, URL */
 import ghpagesAdapter from "svelte-adapter-ghpages"
 import path from "path"
-import svelteMd from "vite-plugin-svelte-md"
-import svelteMdOption from "./docs-svelte-kit/tools/vite-plugin-svelte-md-option.mjs"
-
-import "./docs-svelte-kit/build-system/build.js"
 
 const dirname =
   typeof __dirname !== "undefined"
@@ -41,46 +37,6 @@ const config = {
     },
 
     trailingSlash: "always",
-
-    vite: {
-      server: {
-        fs: { strict: false },
-      },
-      resolve: {
-        alias: {
-          eslint: path.join(dirname, "./docs-svelte-kit/shim/eslint.mjs"),
-          assert: path.join(dirname, "./docs-svelte-kit/shim/assert.mjs"),
-          "postcss-load-config": path.join(
-            dirname,
-            "./docs-svelte-kit/shim/postcss-load-config.mjs",
-          ),
-          "source-map-js": path.join(
-            dirname,
-            "./docs-svelte-kit/shim/source-map-js.mjs",
-          ),
-          module: path.join(dirname, "./docs-svelte-kit/shim/module.mjs"),
-          path: path.join(dirname, "./docs-svelte-kit/shim/path.mjs"),
-          url: path.join(dirname, "./docs-svelte-kit/shim/url.mjs"),
-          os: path.join(dirname, "./docs-svelte-kit/shim/os.mjs"),
-          fs: path.join(dirname, "./docs-svelte-kit/shim/fs.mjs"),
-          globby: path.join(dirname, "./docs-svelte-kit/shim/globby.mjs"),
-          tslib: path.join(dirname, "./node_modules/tslib/tslib.es6.js"),
-        },
-      },
-      plugins: [
-        svelteMd(
-          svelteMdOption({
-            baseUrl: "/eslint-plugin-svelte",
-            root: path.join(dirname, "./docs"),
-          }),
-        ),
-      ],
-      build: {
-        commonjsOptions: {
-          ignoreDynamicRequires: true,
-        },
-      },
-    },
   },
 }
 export default config
---
+++ vite.config.mjs
@@ -0,0 +1,53 @@
+/* global URL -- URL */
+import { sveltekit } from "@sveltejs/kit/vite"
+import path from "path"
+import svelteMd from "vite-plugin-svelte-md"
+import svelteMdOption from "./docs-svelte-kit/tools/vite-plugin-svelte-md-option.mjs"
+
+import "./docs-svelte-kit/build-system/build.js"
+
+const dirname = path.dirname(new URL(import.meta.url).pathname)
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [
+    svelteMd(
+      svelteMdOption({
+        baseUrl: "/eslint-plugin-svelte",
+        root: path.join(dirname, "./docs"),
+      }),
+    ),
+    sveltekit(),
+  ],
+  server: {
+    fs: { strict: false },
+  },
+  resolve: {
+    alias: {
+      eslint: path.join(dirname, "./docs-svelte-kit/shim/eslint.mjs"),
+      assert: path.join(dirname, "./docs-svelte-kit/shim/assert.mjs"),
+      "postcss-load-config": path.join(
+        dirname,
+        "./docs-svelte-kit/shim/postcss-load-config.mjs",
+      ),
+      "source-map-js": path.join(
+        dirname,
+        "./docs-svelte-kit/shim/source-map-js.mjs",
+      ),
+      module: path.join(dirname, "./docs-svelte-kit/shim/module.mjs"),
+      path: path.join(dirname, "./docs-svelte-kit/shim/path.mjs"),
+      url: path.join(dirname, "./docs-svelte-kit/shim/url.mjs"),
+      os: path.join(dirname, "./docs-svelte-kit/shim/os.mjs"),
+      fs: path.join(dirname, "./docs-svelte-kit/shim/fs.mjs"),
+      globby: path.join(dirname, "./docs-svelte-kit/shim/globby.mjs"),
+      tslib: path.join(dirname, "./node_modules/tslib/tslib.es6.js"),
+    },
+  },
+  build: {
+    commonjsOptions: {
+      ignoreDynamicRequires: true,
+    },
+  },
+}
+
+export default config
