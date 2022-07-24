933714f95db64c35f3ee6af16a0021b9b582f44f
yosuke ota <otameshiyo23@gmail.com>
2022-07-24 22:53:39 +0900

    add test

---
+++ tests/fixtures/rules/sort-attributes/invalid/ignore-no-order-test-input.svelte
@@ -0,0 +1,4 @@
+<div order-b order-c order-a />
+<div a b c />
+<div c b a />
+<div h g f e order-b d order-a c order-c b a />
---
+++ tests/fixtures/rules/sort-attributes/invalid/ignore-no-order-test-output.svelte
@@ -0,0 +1,4 @@
+<div order-a order-b order-c />
+<div a b c />
+<div c b a />
+<div h g f e order-a order-b d c order-c b a />
