b060f2517df86dd7100c804bfd928c018b792a90
yosuke ota <otameshiyo23@gmail.com>
2022-07-24 22:58:14 +0900

    add tests

---
+++ tests/fixtures/rules/sort-attributes/valid/ignore-no-order-with-spread-test-input.svelte
@@ -0,0 +1,8 @@
+<script>
+  let attrs
+</script>
+
+<div order-b {...attrs} order-a order-c />
+<div a b {...attrs} c />
+<div c {...attrs} b a />
+<div h g f e order-b d {...attrs} c order-a order-c b a />
