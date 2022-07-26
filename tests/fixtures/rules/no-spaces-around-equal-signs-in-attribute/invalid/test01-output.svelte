1f73a2060a0300f61a26e3c2b875b0c433487ce9
Marek Vospel <73162071+marekvospel@users.noreply.github.com>
2022-07-26 02:26:56 +0200

    feat: add `svelte/no-spaces-around-equal-signs-in-attribute` (#191)
    * feat: add `svelte/no-spaces-around-equal-signs-in-attribute`
    
    Moved to separate PR from ota-meshi/eslint-plugin-svelte#186
    
    * chore: requested changes

---
+++ tests/fixtures/rules/no-spaces-around-equal-signs-in-attribute/invalid/test01-output.svelte
@@ -0,0 +1,15 @@
+<!-- prettier-ignore -->
+<div>
+  <p class="h"></p>
+  <p class="e"></p>
+  <p class="l"></p>
+  <p class="l"></p>
+  <p class="o"></p>
+  <p class="="></p>
+  <p class=a></p>
+  <p class=a></p>
+  <p bind:test={value} bind:test2={value} />
+  <p style:width="10px" />
+  <!-- prettier-ignore -->
+  <p this={expression} />
+</div>
