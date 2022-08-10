2fd72eedc39f7166957cbef5da3a3f2d725dcd10
Pat Cavit <github@patcavit.com>
2022-08-09 23:34:13 -0700

    wip: feedback

--- src/rules/prefer-destructured-store-props.ts
+++ src/rules/prefer-destructured-store-props.ts
@@ -3,6 +3,7 @@ import type { TSESTree } from "@typescript-eslint/types"
 import type { AST } from "svelte-eslint-parser"
 import { createRule } from "../utils"
 import { getStringIfConstant } from "../utils/ast-utils"
+import { returnStatement } from "@babel/types"
 
 type NodeRecord = { property: string; node: TSESTree.MemberExpression }
 
@@ -17,18 +18,31 @@ export default createRule("prefer-destructured-store-props", {
     hasSuggestions: true,
     schema: [],
     messages: {
-      useDestructuring: `Destructure {{property}} from store {{store}} for better change tracking & fewer redraws`,
+      useDestructuring: `Destructure {{property}} from {{store}} for better change tracking & fewer redraws`,
       fixUseDestructuring: `Using destructuring like $: ({ {{property}} } = {{store}}); will run faster`,
     },
     type: "suggestion",
   },
   create(context) {
     let script: AST.SvelteScriptElement
+
+    // Store off instances of probably-destructurable statements
     const reports: NodeRecord[] = []
+
+    // Store off defined variable names so we can avoid offering a suggestion in those cases
+    const defined: Set<string> = new Set()
 
     return {
       [`SvelteScriptElement`](node: AST.SvelteScriptElement) {
         script = node
+      },
+
+      [`VariableDeclarator[id.type="Identifier"]`](
+        node: TSESTree.VariableDeclarator,
+      ) {
+        const { name } = node.id as TSESTree.Identifier
+
+        defined.add(name)
       },
 
       // {$foo.bar}
@@ -54,13 +68,6 @@ export default createRule("prefer-destructured-store-props", {
       [`Program:exit`]() {
         reports.forEach(({ property, node }) => {
           const store = (node.object as TSESTree.Identifier).name
-          // let prop: string | null = null
-
-          // if (node.property.type === "Literal") {
-          //   prop = node.property.value as string
-          // } else if (node.property.type === "Identifier") {
-          //   prop = node.property.name
-          // }
 
           context.report({
             node,
@@ -70,33 +77,42 @@ export default createRule("prefer-destructured-store-props", {
               property,
             },
 
-            suggest: [
-              {
-                messageId: "fixUseDestructuring",
-                data: {
-                  store,
-                  property,
-                },
+            // Avoid suggestions for:
+            //  dynamic accesses like {$foo[bar]}
+            //  no <script> tag
+            //  no <script> ending
+            //  variable name already defined
+            suggest:
+              node.computed ||
+              !script ||
+              !script.endTag ||
+              defined.has(property)
+                ? []
+                : [
+                    {
+                      messageId: "fixUseDestructuring",
+                      data: {
+                        store,
+                        property,
+                      },
 
-                fix(fixer) {
-                  // Avoid autofix suggestions for:
-                  //  dynamic accesses like {$foo[bar]}
-                  //  no <script> tag
-                  //  no <script> ending
-                  if (node.computed || !script || !script.endTag) {
-                    return []
-                  }
+                      fix(fixer) {
+                        // This is only necessary to make TS shut up about script.endTag maybe being null
+                        // but since we already checked it above that warning is just wrong
+                        if (!script.endTag) {
+                          return []
+                        }
 
-                  return [
-                    fixer.insertTextAfterRange(
-                      [script.endTag.range[0], script.endTag.range[0]],
-                      `$: ({ ${property} } = ${store});\n`,
-                    ),
-                    fixer.replaceText(node, property),
-                  ]
-                },
-              },
-            ],
+                        return [
+                          fixer.insertTextAfterRange(
+                            [script.endTag.range[0], script.endTag.range[0]],
+                            `$: ({ ${property} } = ${store});\n`,
+                          ),
+                          fixer.replaceText(node, property),
+                        ]
+                      },
+                    },
+                  ],
           })
         })
       },
---
+++ tests/fixtures/rules/prefer-destructured-store-props/invalid/test03-input.svelte
@@ -0,0 +1,17 @@
+<!-- prettier-ignore -->
+<script>
+  import store from "./store.js";
+	
+  let foo, bar
+  $: {
+    foo = $store.foo;
+    bar = $store.bar;
+  }
+</script>
+
+<div>
+  foo: {foo + " " + Date.now()}
+</div>
+<div>
+  bar: {bar + " " + Date.now()}
+</div>
