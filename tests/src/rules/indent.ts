20dd202a81c0b54ab6f5fe17de93d0766c7eb81c
Yosuke Ota <otameshiyo23@gmail.com>
2022-08-30 12:52:01 +0900

    test: ignore indent test for decorators with ts4.8 (#244)
    * test: ignore indent test for decorators with ts4.8
    
    * Update ci.json
    
    * Update ci.json
    
    * chore: update codesandbox config

--- tests/src/rules/indent.ts
+++ tests/src/rules/indent.ts
@@ -33,6 +33,15 @@ describe("use @typescript-eslint/parser@4", () => {
     rule as any,
     loadTestCases("indent", {
       filter(filename) {
+        if (
+          filename.endsWith("ts-class03-input.svelte") ||
+          filename.endsWith("ts-decorator01-input.svelte") ||
+          filename.endsWith("ts-decorator02-input.svelte")
+        ) {
+          // Decorator nodes in ts4.8 are incompatible.
+          return false
+        }
+
         return path.basename(path.dirname(filename)) === "ts"
       },
     }),
