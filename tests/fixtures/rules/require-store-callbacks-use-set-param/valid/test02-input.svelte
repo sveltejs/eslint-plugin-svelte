1240968523fb7721c6ca3ea5bdf943247be4a099
Yuichiro Yamashita <xydybaseball@gmail.com>
2022-11-01 08:26:43 +0900

    feat: Add `require-store-callbacks-use-set-param` rule (#284)
    * feat: add require-store-callbacks-use-set-param
    
    * chore: add changeset
    
    * chore: tidy up
    
    * fix: update review comments
    
    * fix: review comment

---
+++ tests/fixtures/rules/require-store-callbacks-use-set-param/valid/test02-input.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { readable, writable, derived } from "svelte/store"
+
+  readable(null, function (set) {
+    set(new Date())
+    const interval = setInterval(() => set(new Date()), 1000)
+    return () => clearInterval(interval)
+  })
+
+  readable(false, function (set) {
+    /* do nothing */
+  })
+
+  writable(0, function (set) {
+    return () => {}
+  })
+  writable(null, function (set) {
+    set(0)
+    return () => {}
+  })
+
+  derived(a, function ($a) {
+    /* do nothing */
+  })
+  derived(
+    a,
+    function ($a, set) {
+      setTimeout(() => set($a), 1000)
+    },
+    "one moment...",
+  )
+</script>
