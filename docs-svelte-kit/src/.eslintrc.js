d0d21e75c323bb843bd32227a9abcf1f0bcd798a
Yuichiro Yamashita <xydybaseball@gmail.com>
2022-11-01 01:47:07 +0900

    chore: revert .eslintrc.js

--- docs-svelte-kit/src/.eslintrc.js
+++ docs-svelte-kit/src/.eslintrc.js
@@ -6,7 +6,5 @@ module.exports = {
     "node/no-unsupported-features/es-syntax": "off",
     "require-jsdoc": "off",
     "node/no-missing-import": "off",
-    "@ota-meshi/missing-module-for-config": "off",
-    "prettier/prettier": "off",
   },
 }
