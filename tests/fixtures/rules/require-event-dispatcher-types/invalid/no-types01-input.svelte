c97a85c2b2588ea599d53e236f317f36b0a4351d
Marek Dědič <developer@dedic.eu>
2023-02-01 21:52:02 +0100

    test(require-event-dispatcher-types): Added tests

---
+++ tests/fixtures/rules/require-event-dispatcher-types/invalid/no-types01-input.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte"
+
+  const dispatch1 = createEventDispatcher()
+  const dispatch2 = createEventDispatcher<>()
+</script>
---
+++ tests/fixtures/rules/require-event-dispatcher-types/valid/non-svelte-dispatcher01-input.svelte
@@ -0,0 +1,5 @@
+<script>
+  import { createEventDispatcher } from "./unknown"
+
+  const dispatch = createEventDispatcher()
+</script>
