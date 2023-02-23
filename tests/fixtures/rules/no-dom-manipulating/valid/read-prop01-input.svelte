296add944ce50fff8a4c873b83f674549eba2a56
yosuke ota <otameshiyo23@gmail.com>
2023-02-23 09:29:26 +0900

    fix: false positives for read property in `svelte/no-dom-manipulating`

---
+++ tests/fixtures/rules/no-dom-manipulating/valid/read-prop01-input.svelte
@@ -0,0 +1,8 @@
+<script>
+  let divElement
+  let height = ""
+
+  $: if (divElement) height = `${divElement.clientHeight}px`
+</script>
+
+<div bind:this={divElement} />
---
+++ tests/fixtures/rules/no-dom-manipulating/valid/read-prop02-input.svelte
@@ -0,0 +1,8 @@
+<script>
+  let divElement
+  let foo = ""
+
+  $: if (divElement) foo = `${divElement.data.foo}`
+</script>
+
+<div bind:this={divElement} data-foo="Foo" />
