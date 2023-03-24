8af0192d1502035d3230376a0e0debf365bdb277
yosuke ota <otameshiyo23@gmail.com>
2023-03-24 10:41:37 +0900

    chore: fix type

--- typings/estree/index.d.ts
+++ typings/estree/index.d.ts
@@ -5,6 +5,7 @@
 import type { TSESTree } from "@typescript-eslint/types"
 
 export type Node = TSESTree.Node
+export type Program = TSESTree.Program
 export type Expression = TSESTree.Expression
 export type Statement = TSESTree.Statement
 export type Pattern = TSESTree.Pattern
