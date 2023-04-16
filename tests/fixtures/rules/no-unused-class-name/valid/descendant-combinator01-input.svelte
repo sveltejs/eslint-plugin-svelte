281c28788a25f213bbacd7669de48745be07a3e1
Marek Dědič <developer@dedic.eu>
2023-04-16 18:11:01 +0200

    test(no-unused-class-name): added tests for various selectors

---
+++ tests/fixtures/rules/no-unused-class-name/valid/descendant-combinator01-input.svelte
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="div-class">Hello</div>
+</div>
+
+<style>
+  .container .div-class {
+    color: red;
+  }
+</style>
---
+++ tests/fixtures/rules/no-unused-class-name/valid/pseudo01-input.svelte
@@ -0,0 +1,12 @@
+<div class="div-class">Hello</div>
+
+<span class="span-class">World!</span>
+
+<style>
+  .div-class:visited {
+    color: red;
+  }
+  .span-class::before {
+    color: red;
+  }
+</style>
