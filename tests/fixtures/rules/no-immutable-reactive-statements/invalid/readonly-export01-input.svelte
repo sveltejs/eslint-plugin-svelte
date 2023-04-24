19996f53495b7742624854813db8520b34212ec0
yosuke ota <otameshiyo23@gmail.com>
2023-04-24 10:57:53 +0900

    test: add test case

---
+++ tests/fixtures/rules/no-immutable-reactive-statements/invalid/readonly-export01-input.svelte
@@ -0,0 +1,15 @@
+<script>
+  export const thisIs = "readonly"
+
+  export function greet(name) {
+    console.log(`hello ${name}!`)
+  }
+
+  export class Foo {}
+
+  const immutableVar = "hello"
+
+  $: message1 = greet(immutableVar)
+  $: message2 = `this is${thisIs}`
+  $: instance = new Foo()
+</script>
