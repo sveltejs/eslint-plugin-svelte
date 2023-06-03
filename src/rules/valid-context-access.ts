9e01170e9a2fb84715de24f9dcd1e7427a473f89
baseballyama <xydybaseball@gmail.com>
2023-06-03 16:09:22 +0900

    support await

--- src/rules/valid-context-access.ts
+++ src/rules/valid-context-access.ts
@@ -1,6 +1,7 @@
 import { createRule } from "../utils"
 import { extractContextReferences } from "./reference-helpers/svelte-context"
 import { extractSvelteLifeCycleReferences } from "./reference-helpers/svelte-lifecycle"
+import type { AST } from "svelte-eslint-parser"
 import type { TSESTree } from "@typescript-eslint/types"
 
 export default createRule("valid-context-access", {
@@ -30,19 +31,20 @@ export default createRule("valid-context-access", {
     ).map((r) => r.node)
 
     // Extract <script> blocks that is not module=context.
-    const scriptNotModuleElements = sourceCode.ast.body.filter((b) => {
-      if (b.type !== "SvelteScriptElement") return false
-      const isModule = b.startTag.attributes.some((a) => {
-        return (
-          a.type === "SvelteAttribute" &&
-          a.key.name === "context" &&
-          a.value.some(
-            (v) => v.type === "SvelteLiteral" && v.value === "module",
+    const scriptNotModuleElements: AST.SvelteScriptElement[] =
+      sourceCode.ast.body.filter((b) => {
+        if (b.type !== "SvelteScriptElement") return false
+        const isModule = b.startTag.attributes.some((a) => {
+          return (
+            a.type === "SvelteAttribute" &&
+            a.key.name === "context" &&
+            a.value.some(
+              (v) => v.type === "SvelteLiteral" && v.value === "module",
+            )
           )
-        )
-      })
-      return !isModule
-    })
+        })
+        return !isModule
+      }) as AST.SvelteScriptElement[]
 
     const scopeManager = sourceCode.scopeManager
     const toplevelScope =
@@ -78,20 +80,44 @@ export default createRule("valid-context-access", {
       return []
     }
 
+    /** Return true if tnodeA is inside of nodeB. */
+    function isInsideOfNodeB(
+      nodeA: TSESTree.Node | AST.SvelteScriptElement,
+      nodeB: TSESTree.Node,
+    ) {
+      return (
+        nodeA.range[0] <= nodeB.range[0] && nodeB.range[1] <= nodeA.range[1]
+      )
+    }
+
     /** Return true if the node is there inside of <script> block that is not module=context. */
     function isInsideOfSvelteScriptElement(node: TSESTree.Node) {
       for (const script of scriptNotModuleElements) {
-        if (
-          node.range[0] >= script.range[0] &&
-          node.range[1] <= script.range[1]
-        ) {
+        if (isInsideOfNodeB(script, node)) {
           return true
         }
       }
       return false
     }
 
-    const awaitExpressions: TSESTree.AwaitExpression[] = []
+    const awaitExpressions: {
+      belongingFunction:
+        | TSESTree.FunctionDeclaration
+        | TSESTree.VariableDeclaration
+      node: TSESTree.Node
+    }[] = []
+
+    /** Return true if the given node is later than the await expression. */
+    function isAfterAwait(node: TSESTree.CallExpression) {
+      for (const awaitExpression of awaitExpressions) {
+        const { belongingFunction, node: awaitNode } = awaitExpression
+        if (isInsideOfNodeB(node, belongingFunction)) {
+          continue
+        }
+        return awaitNode.range[0] <= node.range[0]
+      }
+      return false
+    }
 
     /** Let's lint! */
     function doLint(
@@ -101,6 +127,11 @@ export default createRule("valid-context-access", {
     ) {
       // Report if context function is called outside of <script> block.
       if (!isInsideOfSvelteScriptElement(currentNode)) {
+        report(contextCallExpression)
+        return
+      }
+
+      if (isAfterAwait(currentNode)) {
         report(contextCallExpression)
         return
       }
@@ -151,7 +182,15 @@ export default createRule("valid-context-access", {
         }
       },
       AwaitExpression(node) {
-        awaitExpressions.push(node)
+        let parent: TSESTree.Node | undefined = node.parent
+        while (parent) {
+          if (parent.type === "FunctionDeclaration") {
+            awaitExpressions.push({ belongingFunction: parent, node })
+          } else if (parent.type === "VariableDeclaration") {
+            awaitExpressions.push({ belongingFunction: parent, node })
+          }
+          parent = parent.parent
+        }
       },
     }
   },
