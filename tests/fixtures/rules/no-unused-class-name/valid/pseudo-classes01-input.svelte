d8ed1a699946ea2de385064acaaf1a9c6d24049f
Marek Dědič <developer@dedic.eu>
2023-06-18 15:23:44 +0200

    test(no-unused-class-name): added a test with pseudo-classes

---
+++ tests/fixtures/rules/no-unused-class-name/valid/pseudo-classes01-input.svelte
@@ -0,0 +1,14 @@
+<div class="div-class">Hello</div>
+
+<span class="span-class-one">World</span>
+
+<span class="span-class-two">!</span>
+
+<style>
+  :not(.div-class) {
+    color: red;
+  }
+  :is(.span-class-one, .span-class-two) {
+    color: red;
+  }
+</style>
