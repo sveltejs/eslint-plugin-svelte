58eaa192701acc6944f119d9406162d9891f9b77
Marek Dědič <developer@dedic.eu>
2023-11-03 11:40:17 +0100

    feat(no-inline-styles): added rule implementation

---
+++ src/rules/no-inline-styles.ts
@@ -0,0 +1,34 @@
+import { createRule } from '../utils';
+
+export default createRule('no-inline-styles', {
+	meta: {
+		docs: {
+			description: 'disallow properties and directives that produce inline styles',
+			category: 'Best Practices',
+			recommended: false
+		},
+		schema: [],
+		messages: {
+			hasStyleAttribute: 'Found disallowed style attribute.',
+			hasStyleDirective: 'Found disallowed style directive.'
+		},
+		type: 'suggestion'
+	},
+	create(context) {
+		return {
+			SvelteElement(node) {
+				if (node.kind !== 'html') {
+					return;
+				}
+				for (const attribute of node.startTag.attributes) {
+					if (attribute.type === 'SvelteStyleDirective') {
+						context.report({ loc: attribute.loc, messageId: 'hasStyleDirective' });
+					}
+					if (attribute.type === 'SvelteAttribute' && attribute.key.name === 'style') {
+						context.report({ loc: attribute.loc, messageId: 'hasStyleAttribute' });
+					}
+				}
+			}
+		};
+	}
+});
