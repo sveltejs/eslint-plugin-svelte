ca39dcd80f161b48e1d621dbb9f10128d966640f
Yosuke Ota <otameshiyo23@gmail.com>
2023-11-20 18:15:29 +0900

    chore: fix ci error (#621)

---
+++ tests/fixtures/rules/indent/invalid/ts-v5/ts-import-attributes04-input.svelte
@@ -0,0 +1,12 @@
+<!-- prettier-ignore -->
+<script lang="ts">
+export { foo } from "mod" with
+{ 
+type
+:
+"json"
+}
+;
+</script>
+
+<!--tests/fixtures/rules/indent/invalid/ts/ts-import-attributes04-input-->
