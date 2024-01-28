e6615fdaf4bf5a5ba35a2a0dfdca7830fb17f3d5
Marek Dědič <developer@dedic.eu>
2024-01-28 14:49:53 +0100

    test(no-goto-without-base): added tests for the rule

---
+++ tests/fixtures/rules/no-goto-without-base/valid/base-aliased01-input.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { base as alias } from "$app/paths";
+  import { goto } from "$app/navigation";
+
+  goto(alias + "/foo/");
+  goto(`${alias}/foo/`);
+</script>
---
+++ tests/fixtures/rules/no-goto-without-base/valid/base-prefixed01-input.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { base } from "$app/paths";
+  import { goto } from "$app/navigation";
+
+  goto(base + "/foo/");
+  goto(`${base}/foo/`);
+</script>
