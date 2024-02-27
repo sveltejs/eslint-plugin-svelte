e896d7af3b9c17ea6d1197e38f36fe0a9a8ce61f
Stephen Darnell <stephen.darnell@outlook.com>
2024-02-27 19:43:02 +0000

    Fix lint issues

--- tests/fixtures/rules/prefer-class-directive/valid/empty/ignore-test05-input.svelte
+++ tests/fixtures/rules/prefer-class-directive/valid/empty/ignore-test05-input.svelte
@@ -4,7 +4,7 @@
 	let danger = false;
 </script>
 
-<button class="{a ? 'a' : 'b'}">foo</button>
-<button class="{!a ? 'b' : 'a'}">foo</button>
-<button class="{a7 === 7 ? 'a' : 'b'}">foo</button>
+<button class={a ? 'a' : 'b'}>foo</button>
+<button class={!a ? 'b' : 'a'}>foo</button>
+<button class={a7 === 7 ? 'a' : 'b'}>foo</button>
 <button class="btn {danger ? 'btn-danger' : 'btn-primary'}">foo</button>
