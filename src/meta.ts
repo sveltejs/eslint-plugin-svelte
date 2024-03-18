b6482671b48a10b088610becbdfdbc563e31376f
ota-meshi <otameshiyo23@gmail.com>
2024-03-19 07:09:40 +0900

    fix

--- src/meta.ts
+++ src/meta.ts
@@ -1,5 +1,5 @@
 // IMPORTANT!
 // This file has been automatically generated,
 // in order to update its content execute "pnpm run update"
-export const name = 'eslint-plugin-svelte' as const;
-export const version = '2.36.0-next.11' as const;
+export const name = 'eslint-plugin-svelte';
+export const version = '2.36.0-next.11';
