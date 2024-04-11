33f0de261636e0dca77ec0479fa93a988e6ca950
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-04-11 10:32:36 -0400

    feat: add rule types (#735)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './rule-types';
 import type { RuleModule } from './types';
 import { rules as ruleList } from './utils/rules';
 import base from './configs/base';
---
+++ tools/update-rule-types.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import plugin from '../src/index';
+
+void main();
+
+async function main() {
+	const { pluginsToRulesDTS } = await import('eslint-typegen/core');
+
+	// @ts-expect-error - types are a bit strict here
+	const ruleTypes = await pluginsToRulesDTS({ svelte: plugin });
+
+	void fs.writeFileSync(
+		path.join(__dirname, '../src/rule-types.ts'),
+		`// IMPORTANT!
+// This file has been automatically generated,
+// in order to update its content execute "pnpm run update"
+
+${ruleTypes}`
+	);
+}
