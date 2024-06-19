f1036ce5db1ac3b7d655d4a8ec56f2ad1b9d3bda
yosuke ota <otameshiyo23@gmail.com>
2024-06-19 17:42:31 +0900

    add test

---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/indent/invalid/script-binary03-output.svelte
@@ -0,0 +1,11 @@
+<!-- prettier-ignore -->
+<script>
+  if (
+    a === b ||
+    c === d ||
+    e === f
+  ) {
+  }
+</script>
+
+<!--tests/fixtures/rules/indent/invalid/script-binary03-input.svelte-->
