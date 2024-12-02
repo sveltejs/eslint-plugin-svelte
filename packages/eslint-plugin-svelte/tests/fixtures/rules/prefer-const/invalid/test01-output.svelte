ff33b0e7418dfaf1f29b720c6823e80b60f083ab
Miquel de Domingo <miquelddg@gmail.com>
2024-12-02 09:04:51 +0100

    fix(prefer-const): remove `$state` from excluded runes

--- packages/eslint-plugin-svelte/tests/fixtures/rules/prefer-const/invalid/test01-output.svelte
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/prefer-const/invalid/test01-output.svelte
@@ -1,7 +1,7 @@
 <script>
 	let { prop1, prop2 } = $props();
 	const zero = 0;
-	let state = $state(0);
+	const state = $state(0);
 	let raw = $state.raw(0);
 	const doubled = state * 2;
 	let derived = $derived(state * 2);
