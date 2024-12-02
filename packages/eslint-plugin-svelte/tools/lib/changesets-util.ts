d3fd17cbb4907468be35e9b33164982a18aa3254
yosuke ota <otameshiyo23@gmail.com>
2024-12-02 09:18:27 +0900

    fix

--- packages/eslint-plugin-svelte/tools/lib/changesets-util.ts
+++ packages/eslint-plugin-svelte/tools/lib/changesets-util.ts
@@ -1,6 +1,8 @@
 import getReleasePlan from '@changesets/get-release-plan';
 import path from 'path';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 /** Get new version string from changesets */
 export async function getNewVersion(): Promise<string> {
 	const releasePlan = await getReleasePlan(path.resolve(__dirname, '../../../..'));
