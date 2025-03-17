1ff148a0bb24954aef59ce93b05a35913d83f2ee
Yuichiro Yamashita <xydybaseball@gmail.com>
2025-03-17 23:04:56 +0900

    fix: correct detection of externally defined types in `no-unused-props` rule (#1135)

---
+++ .changeset/plain-chairs-tie.md
@@ -0,0 +1,5 @@
+---
+'eslint-plugin-svelte': patch
+---
+
+fix: correct detection of externally defined types in `no-unused-props` rule
---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-unused-props/valid/ignore-external-type-input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { FooDTO } from './shared-types';
+
+	interface Props extends FooDTO {
+		child1: string;
+		child2: number;
+	}
+
+	let { foo, child1, child2 }: Props = $props();
+</script>
