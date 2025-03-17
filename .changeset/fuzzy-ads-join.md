e94b59a35c914d868f89dd300b8d6b8090054976
Yuichiro Yamashita <xydybaseball@gmail.com>
2025-03-17 17:49:50 +0900

    fix: resolve false positives on nested objects in `no-unused-props` rule (#1128)

---
+++ .changeset/fuzzy-ads-join.md
@@ -0,0 +1,5 @@
+---
+'eslint-plugin-svelte': patch
+---
+
+fix: resolve false positives on nested objects in `no-unused-props` rule
---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-unused-props/valid/ts-basic-input.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	// fix: https://github.com/sveltejs/eslint-plugin-svelte/issues/1028#issuecomment-2728101827
+	type Prop = {
+		value: string;
+		value2: string;
+	};
+
+	interface Props {
+		myObjectProp: Prop;
+	}
+
+	const { myObjectProp }: Props = $props();
+</script>
+
+<p>{myObjectProp.value} {myObjectProp.value2}</p>
