2c74993e3427c30fd6af6c3189843a7093e2200a
yosuke ota <otameshiyo23@gmail.com>
2025-04-30 16:06:12 +0900

    chore: run update

--- packages/eslint-plugin-svelte/src/meta.ts
+++ packages/eslint-plugin-svelte/src/meta.ts
@@ -1,5 +1,5 @@
 // IMPORTANT!
 // This file has been automatically generated,
 // in order to update its content execute "pnpm run update"
-export const name = 'eslint-plugin-svelte';
-export const version = '3.5.1';
+export const name = 'eslint-plugin-svelte' as const;
+export const version = '3.5.1' as const;
