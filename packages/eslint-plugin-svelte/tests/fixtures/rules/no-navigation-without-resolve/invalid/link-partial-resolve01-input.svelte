9223298e31baf9266719dcc25195518ebe116825
Marek Dědič <developer@dedic.eu>
2025-08-07 20:16:05 +0200

    test(no-navigation-without-resolve): added rule tests

---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/invalid/link-partial-resolve01-input.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { resolve } from '$app/paths';
+</script>
+
+<a href={resolve('/foo') + '/bar'}>Click me!</a>
+<a href={'/foo' + resolve('/bar')}>Click me!</a>
---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/invalid/pushState-partial-resolve01-input.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { resolve } from '$app/paths';
+	import { pushState } from '$app/navigation';
+
+	pushState(resolve('/foo') + '/bar');
+	pushState('/foo' + resolve('/bar'));
+</script>
