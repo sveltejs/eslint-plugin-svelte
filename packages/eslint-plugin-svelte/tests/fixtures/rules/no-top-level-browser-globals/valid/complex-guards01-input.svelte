77f75f6b6e5ab735ffbad9b688e547cadf576179
yosuke ota <otameshiyo23@gmail.com>
2025-09-03 14:06:13 +0900

    fix(no-top-level-browser-globals): false positives for compound logical expression guards

---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-top-level-browser-globals/valid/complex-guards01-input.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { browser, dev } from '$app/environment';
+</script>
+
+{#if browser && dev}
+	<div>{localStorage.getItem('myCat')}</div>
+{/if}
---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-top-level-browser-globals/valid/complex-guards02-input.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { browser, dev } from '$app/environment';
+</script>
+
+{#if !browser || !dev}
+	<!-- (!(browser && dev))  -->
+	<div>DEV</div>
+{:else}
+	<!-- (browser && dev) -->
+	<div>{localStorage.getItem('myCat')}</div>
+{/if}
