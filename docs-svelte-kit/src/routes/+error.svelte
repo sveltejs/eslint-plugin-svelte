19a1c4d67f66bf74cdbf2a503ff9a16ca2842245
yosuke ota <otameshiyo23@gmail.com>
2025-09-03 19:59:14 +0900

    fix

--- docs-svelte-kit/src/routes/+error.svelte
+++ docs-svelte-kit/src/routes/+error.svelte
@@ -6,5 +6,5 @@
 <h1>{$page.status}</h1>
 <blockquote>
 	<p>{$page.error.message}</p>
-	<p>Take me <a href={resolve('')}>home</a></p>
+	<p>Take me <a href={resolve('/')}>home</a></p>
 </blockquote>
--- docs-svelte-kit/src/routes/404/+page.svelte
+++ docs-svelte-kit/src/routes/404/+page.svelte
@@ -5,7 +5,7 @@
 <h1>404</h1>
 <blockquote>
 	<p>Not Found</p>
-	<p>Take me <a href={resolve('')}>home</a></p>
+	<p>Take me <a href={resolve('/')}>home</a></p>
 </blockquote>
 
 <style>
