7e53e33f11c63f84f19e72b4a68916ef53cf86c2
Marek Dědič <developer@dedic.eu>
2025-09-04 15:12:26 +0200

    test(no-navigation-without-resolve): added tests with shorthand properties

--- packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/valid/link-absolute-url01-input.svelte
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/valid/link-absolute-url01-input.svelte
@@ -1,5 +1,8 @@
 <script>
 	const protocol = 'https';
+
+	const value = "https://svelte.dev";
+	const href = "https://svelte.dev";
 </script>
 
 <a href="http://svelte.dev">Click me!</a>
@@ -13,3 +16,5 @@
 <a href={`${protocol}://svelte.dev`}>Click me!</a>
 <a href="mailto:user@example.com">Click me!</a>
 <a href="tel:+123456789">Click me!</a>
+<a href={value}>Click me!</a>
+<a {href}>Click me!</a>
