7b1fca9e4613768f554687cde1b177d8c97ce971
Marek Dědič <developer@dedic.eu>
2025-09-04 15:12:26 +0200

    test(no-navigation-without-resolve): added tests with shorthand properties

--- packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/invalid/link-with-fragment01-input.svelte
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/invalid/link-with-fragment01-input.svelte
@@ -1,9 +1,11 @@
 <script>
 	const value = "/foo#section";
+	const href = "/foo#section";
 </script>
 
 <a href="/foo#section">Click me!</a>
 <a href={'/foo#section'}>Click me!</a>
 <a href={'/' + 'foo#section'}>Click me!</a>
 <a href={value}>Click me!</a>
+<a {href}>Click me!</a>
 <a href={'/foo#section:42'}>Click me!</a>
