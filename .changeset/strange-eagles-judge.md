7cb36605784dd689379600462dbe41f7940ce79a
Yosuke Ota <otameshiyo23@gmail.com>
2025-09-07 19:01:30 +0900

    fix(no-unused-props): false positives for `ComponentProps<any>` (#1306)

---
+++ .changeset/strange-eagles-judge.md
@@ -0,0 +1,5 @@
+---
+"eslint-plugin-svelte": patch
+---
+
+fix(no-unused-props): false positives for `ComponentProps<any>`
---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-unused-props/valid/component-props-any-input.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte';
+
+	interface Props extends ComponentProps<any> {
+		a: string;
+	}
+
+	let { a }: Props = $props();
+</script>
+
+<p>{a}</p>
