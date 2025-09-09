c2d7877972a9811f8d23acc030198ffc8e618dd0
Marek Dědič <developer@dedic.eu>
2025-09-09 18:30:41 +0200

    fix(no-navigation-without-base): fixed an infinite loop

--- packages/eslint-plugin-svelte/src/utils/expression-affixes.ts
+++ packages/eslint-plugin-svelte/src/utils/expression-affixes.ts
@@ -4,77 +4,43 @@ import type { RuleContext } from '../types.js';
 import type { AST } from 'svelte-eslint-parser';
 import { ASTSearchHelper } from './ast-search-helper.js';
 
-// Variable prefix extraction
-
 export function extractExpressionPrefixVariable(
 	context: RuleContext,
 	expression: TSESTree.Expression
 ): TSESTree.Identifier | null {
-	switch (expression.type) {
-		case 'BinaryExpression':
-			return extractBinaryExpressionPrefixVariable(context, expression);
-		case 'Identifier':
-			return extractVariablePrefixVariable(context, expression);
-		case 'MemberExpression':
-			return extractMemberExpressionPrefixVariable(expression);
-		case 'TemplateLiteral':
-			return extractTemplateLiteralPrefixVariable(context, expression);
-		default:
+	return ASTSearchHelper(expression, {
+		BinaryExpression: (node, searchAnotherNode) =>
+			node.left.type !== 'PrivateIdentifier' ? searchAnotherNode(node.left) : null,
+		Identifier: (node, searchAnotherNode) => {
+			const variable = findVariable(context, node);
+			if (
+				variable === null ||
+				variable.identifiers.length !== 1 ||
+				variable.identifiers[0].parent.type !== 'VariableDeclarator' ||
+				variable.identifiers[0].parent.init === null
+			) {
+				return node;
+			}
+			return searchAnotherNode(variable.identifiers[0].parent.init) ?? node;
+		},
+		MemberExpression: (node) => (node.property.type === 'Identifier' ? node.property : null),
+		TemplateLiteral: (node, searchAnotherNode) => {
+			const literalParts = [...node.expressions, ...node.quasis].sort((a, b) =>
+				a.range[0] < b.range[0] ? -1 : 1
+			);
+			for (const part of literalParts) {
+				if (part.type === 'TemplateElement' && part.value.raw === '') {
+					// Skip empty quasi in the begining
+					continue;
+				}
+				if (part.type !== 'TemplateElement') {
+					return searchAnotherNode(part);
+				}
+				return null;
+			}
 			return null;
-	}
-}
-
-function extractBinaryExpressionPrefixVariable(
-	context: RuleContext,
-	expression: TSESTree.BinaryExpression
-): TSESTree.Identifier | null {
-	return expression.left.type !== 'PrivateIdentifier'
-		? extractExpressionPrefixVariable(context, expression.left)
-		: null;
-}
-
-function extractVariablePrefixVariable(
-	context: RuleContext,
-	expression: TSESTree.Identifier
-): TSESTree.Identifier | null {
-	const variable = findVariable(context, expression);
-	if (
-		variable === null ||
-		variable.identifiers.length !== 1 ||
-		variable.identifiers[0].parent.type !== 'VariableDeclarator' ||
-		variable.identifiers[0].parent.init === null
-	) {
-		return expression;
-	}
-	return (
-		extractExpressionPrefixVariable(context, variable.identifiers[0].parent.init) ?? expression
-	);
-}
-
-function extractMemberExpressionPrefixVariable(
-	expression: TSESTree.MemberExpression
-): TSESTree.Identifier | null {
-	return expression.property.type === 'Identifier' ? expression.property : null;
-}
-
-function extractTemplateLiteralPrefixVariable(
-	context: RuleContext,
-	expression: TSESTree.TemplateLiteral
-): TSESTree.Identifier | null {
-	const literalParts = [...expression.expressions, ...expression.quasis].sort((a, b) =>
-		a.range[0] < b.range[0] ? -1 : 1
-	);
-	for (const part of literalParts) {
-		if (part.type === 'TemplateElement' && part.value.raw === '') {
-			// Skip empty quasi in the begining
-			continue;
 		}
-		if (part.type !== 'TemplateElement') {
-			return extractExpressionPrefixVariable(context, part);
-		}
-		return null;
-	}
-	return null;
+	});
 }
 
 export function extractExpressionPrefixLiteral(
