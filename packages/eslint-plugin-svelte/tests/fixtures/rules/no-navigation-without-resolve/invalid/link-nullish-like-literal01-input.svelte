dbb71c5bd31c7afb30c72aa1be0151cccd5e2c97
Marek Dědič <developer@dedic.eu>
2025-10-05 15:28:53 +0200

    fix(no-navigation-without-resolve): allowing undefined and null in link hrefs

---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/invalid/link-nullish-like-literal01-input.svelte
@@ -0,0 +1,15 @@
+<script>
+	const one = "undefined";
+	const two = "null";
+</script>
+
+<a href="undefined">Click me!</a>
+<a href="null">Click me!</a>
+<a href={one}>Click me!</a>
+<a href={two}>Click me!</a>
+<a href={`undefined`}>Click me!</a>
+<a href={`null`}>Click me!</a>
+<a href={`${undefined}`}>Click me!</a>
+<a href={`${null}`}>Click me!</a>
+<a href={`${one}`}>Click me!</a>
+<a href={`${two}`}>Click me!</a>
