89d030dd61dd3ec3240c53dfcfac203da0c543f3
Yosuke Ota <otameshiyo23@gmail.com>
2025-12-04 17:11:55 +0900

    fix(svelte/indent): crash when using ts import type (#1437)

---
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/indent/valid/ts/ts-import-type01-input.svelte
@@ -0,0 +1,12 @@
+<!-- prettier-ignore -->
+<script lang="ts">
+  type Props = {
+    children: import('svelte').Snippet,
+  }
+
+  const { children }: Props = $props();
+</script>
+
+{@render children()}
+
+<!-- tests/fixtures/rules/indent/valid/ts/ts-import-type01-input.svelte -->
