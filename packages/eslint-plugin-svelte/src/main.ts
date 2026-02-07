746ba48f7d3a918f0b91423cf1486e14c5e3f39d
yosuke ota <otameshiyo23@gmail.com>
2026-02-07 17:20:11 +0900

    fix types

--- packages/eslint-plugin-svelte/src/main.ts
+++ packages/eslint-plugin-svelte/src/main.ts
@@ -7,19 +7,21 @@ import prettier from './configs/flat/prettier.js';
 import all from './configs/flat/all.js';
 import * as processor from './processor/index.js';
 import * as metaModule from './meta.js';
-import type { Rule } from 'eslint';
+import type { Linter, Rule } from 'eslint';
 
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion -- ts(2742) Error */
 export const configs = {
-	base,
-	recommended,
-	prettier,
-	all,
+	base: base as Linter.Config[],
+	recommended: recommended as Linter.Config[],
+	prettier: prettier as Linter.Config[],
+	all: all as Linter.Config[],
 	// For backward compatibility
-	'flat/base': base,
-	'flat/recommended': recommended,
-	'flat/prettier': prettier,
-	'flat/all': all
+	'flat/base': base as Linter.Config[],
+	'flat/recommended': recommended as Linter.Config[],
+	'flat/prettier': prettier as Linter.Config[],
+	'flat/all': all as Linter.Config[]
 };
+/* eslint-enable @typescript-eslint/no-unnecessary-type-assertion -- ts(2742) Error */
 
 export const rules = ruleList.reduce(
 	(obj, r) => {
--- packages/eslint-plugin-svelte/tools/update-rule-types.ts
+++ packages/eslint-plugin-svelte/tools/update-rule-types.ts
@@ -6,14 +6,24 @@ void main();
 async function main() {
 	const { pluginsToRulesDTS } = await import('eslint-typegen/core');
 
-	// @ts-expect-error - types are a bit strict here
-	const ruleTypes = await pluginsToRulesDTS({ svelte: plugin });
+	const ruleTypes = await pluginsToRulesDTS(
+		{ svelte: plugin },
+		{ includeAugmentation: false, includeTypeImports: false }
+	);
 
 	void fs.writeFileSync(
 		new URL('../src/rule-types.ts', import.meta.url),
 		`// IMPORTANT!
 // This file has been automatically generated,
 // in order to update its content execute "pnpm run update"
+
+/* eslint-disable */
+/* prettier-ignore */
+import type { Linter } from 'eslint'
+
+declare module 'eslint' {
+	interface RulesConfig extends RuleOptions {}
+}
 
 ${ruleTypes}`
 	);
