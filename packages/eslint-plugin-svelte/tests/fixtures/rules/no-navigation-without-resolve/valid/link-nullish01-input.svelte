d81b556a4ab9399db1a4c464fb647f9bdf97aa32
Marek Dědič <developer@dedic.eu>
2026-02-17 06:46:05 +0100

    fix(no-navigation-without-resolve): fixed nullish link shorthands not being allowed (#1467)

--- packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/valid/link-nullish01-input.svelte
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/valid/link-nullish01-input.svelte
@@ -1,9 +1,11 @@
 <script>
 	const one = undefined;
 	const two = null;
+	const href = null;
 </script>
 
 <a href={undefined}>Click me!</a>
 <a href={null}>Click me!</a>
 <a href={one}>Click me!</a>
 <a href={two}>Click me!</a>
+<a {href}>Click me!</a>
