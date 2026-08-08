a6c49254027a09226b05ba0c2a30cb4a09c5fc62
ColumbusLabs <287001685+ColumbusLabs@users.noreply.github.com>
2026-08-07 22:26:37 -0400

    test: remove non-fixable output fixture

--- packages/eslint-plugin-svelte/tests/fixtures/rules/prefer-attribute-interpolation/invalid/template-literal-attribute-output.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	let foo = 'foo';
-	let bar = 'bar';
-</script>
-
-<Foo attr={`prefix${foo}`} />
-<div data-text={`prefix${foo}${bar}`} />
-<!-- prettier-ignore -->
-<Foo attr="{`prefix${foo}`}" />
